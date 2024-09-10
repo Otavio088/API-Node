cd2059a03abc15873e8035e5ca012293a983ed77
Otavio088 <otavio208augustoribeiro@gmail.com>
2024-09-10 16:40:21 -0300

    Router.js class created for storing routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const router = require('./router');
+
 const app = express();
 
-app.get('/tasks', (request, response) => response.status(200).send("Hello World!"));
+app.use(router);
 
 module.exports = app;
 
