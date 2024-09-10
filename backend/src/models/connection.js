7b2a8104f85e49f03db99667d278c8aedc3293a9
Otavio088 <otavio208augustoribeiro@gmail.com>
2024-09-10 17:00:02 -0300

    Models folder created with classes connection.js and tasksModel.js for communicating with the database

---
+++ backend/src/models/connection.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool(
+    {
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'todolist'
+    }
+);
+
+module.exports = connection;
