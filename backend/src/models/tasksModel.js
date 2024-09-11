4ca82c8e29f07d8c1993ff072394b7abd9db1c53
Otavio088 <otavio208augustoribeiro@gmail.com>
2024-09-11 15:18:25 -0300

    First route with database finished

--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -1,7 +1,10 @@
-const getAll = () => {
+const connection = require('./connection');
 
+const getAll = async () => {
+    const [tasks] = await connection.execute('SELECT * FROM tasks'); //pega a primeira posição do array e retorna na const tasks
+    return tasks;
 };
 
 module.exports = {
     getAll
-}
+};
