0cf6bb16c24a5252079984c4b0f5d139d5a2d8f6
Otavio088 <otavio208augustoribeiro@gmail.com>
2024-09-11 16:59:51 -0300

    Creating and implementing post and delete routes

--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -5,6 +5,24 @@ const getAll = async () => {
     return tasks;
 };
 
+const createTask = async(task) => {
+    const { title } = task;
+    const dateUTC = new Date(Date.now()).toUTCString();
+
+    const query = 'INSERT INTO tasks(title, status, created_at) VALUES (?, ?, ?)';
+
+    const [createdTask] = await connection.execute(query, [title, 'pendente', dateUTC]);
+
+    return {insertId: createdTask.insertId};
+};
+
+const deleteTask = async (id) => {
+    const removedTask = await connection.execute('DELETE FROM tasks WHERE id = ?', [id]);
+    return removedTask;
+};
+
 module.exports = {
-    getAll
+    getAll,
+    createTask,
+    deleteTask
 };
--- backend/src/router.js
+++ backend/src/router.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const tasksController = require('./controllers/tasksController');
 
 const router = express.Router();
 
+const tasksController = require('./controllers/tasksController');
+const tasksMiddleware = require('./middlewares/tasksMiddleware');
+
 router.get('/tasks', tasksController.getAll);
+router.post('/tasks', tasksMiddleware.validateBody, tasksController.createTask);
+router.delete('/tasks/:id', tasksController.deleteTask);
+
 
 module.exports = router;
